e7f8230af5faf8c8105341eb552369bf00633525
ShaliniAvindya <132177735+ShaliniAvindya@users.noreply.github.com>
2025-12-18 08:38:12 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,36 @@
 const mongoose = require('mongoose');
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected:: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error connecting to MongoDB:: ${error.message}`);
-    process.exit(1);
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    const MONGO_URI = process.env.MONGO_URI;
+
+    if (!MONGO_URI) {
+      throw new Error('MONGO_URI is not defined');
+    }
+
+    cached.promise = mongoose.connect(MONGO_URI, {
+      bufferCommands: false,
+    }).then((mongoose) => {
+      console.log('✅ MongoDB connected');
+      return mongoose;
+    }).catch(err => {
+      cached.promise = null;
+      throw err;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 module.exports = connectDB;
